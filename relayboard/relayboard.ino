98bab3b0083432b05018f1d27794a9fe692c1c11
k0f <krystof.pesek@gmail.com>
2016-07-23 16:50:29 -0600

    serial setup

---
+++ relayboard/relayboard.ino
@@ -0,0 +1,53 @@
+
+#define BUTT_TIME  500
+
+char mesg[10];
+int char_i =0;
+long ser_timeo; // incoming serial timeout
+
+long proj_adv; //holds time pressing advance button on projector
+
+long now_t, prev_t, delta_t; // timekeepers
+void setup() {
+  for(int i=2;i<9;i++){pinMode(i, OUTPUT);}
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  now_t = millis();
+  delta_t = now_t - prev_t;
+
+  if(proj_adv > 0){ // check for projector button press time
+    proj_adv += delta_t;
+    if(proj_adv > BUTT_TIME){digitalWrite(5, LOW); proj_adv = 0;}
+  }
+
+  ser_timeo += delta_t;
+  if (char_i > 0 && ser_timeo > 50){char_i = 0;} // reset serial indx
+  if (Serial.available() > 0) {
+    mesg[char_i] = Serial.read();
+    char_i++;
+    ser_timeo =0;
+  }
+
+  if(char_i == 8 ){
+    Serial.println("8");
+    for(int i=0;i<10;i++){Serial.print(mesg[i]);}
+    Serial.println("");
+    char_i =0;
+    activ();
+  }
+
+  prev_t = now_t; //time marches on
+}// end loop
+
+void activ(){
+  for(int i=2,y=1;i<9;i++,y++){
+    if(y == 4){if(mesg[y] =='1'){digitalWrite(5,HIGH);proj_adv =1;}}
+    else{
+      if(mesg[y] == '1'){digitalWrite(i, HIGH);}
+      else if(mesg[y] == '2'){digitalWrite(i, LOW);}
+    }
+  }
+}
