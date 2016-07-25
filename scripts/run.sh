fb0befd22b039e132fea00459adddeb0ac9317c3
k0f <krystof.pesek@gmail.com>
2016-07-25 10:13:40 -0600

    testing mode

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-cat /dev/ttyACM0 &> /dev/null
+(cat /dev/ttyACM0 &> /dev/null)&
 echo "hello world" > ~/log.txt
-(omxplayer --loop --no-osd -b -ohdmi video.mp4 < /home/pi/ctl)&
-echo -n i > /home/pi/ctl
+(/usr/bin/omxplayer --loop --no-osd -b -ohdmi video.mp4 < /home/pi/ctl)&
+(echo -n i > /home/pi/ctl)&
+
+bash /home/pi/serialOn.sh
+source /home/pi/master.sh
+
+
+# TESTING MODE ################
+
+(while true; do 
+	sleep 10
+	shieldsUp
+	carouselOn
+	sleep 5
+	for i in `seq 1 80`; do carouselAdvance; sleep 1 ; done
+	carouselOff
+	shieldsDown
+	allOff
+	sleep 60
+done)&
