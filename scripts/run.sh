6e45305bddc7d60d31c785b2e39759d4cf3ab1ab
k0f <krystof.pesek@gmail.com>
2016-07-26 12:22:04 -0600

    testing sequence fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-
+(sudo touch /forcefsck)&
 (cat /dev/ttyACM0 &> /dev/null)&
+
 echo "hello world" > ~/log.txt
+
 (/usr/bin/omxplayer --loop --no-osd -b -ohdmi video.mp4 < /home/pi/ctl)&
 (echo -n i > /home/pi/ctl)&
 
 bash /home/pi/serialOn.sh
 source /home/pi/master.sh
 
-
-# TESTING MODE ################
-
 (while true; do 
 	sleep 10
 	shieldsUp
 	carouselOn
-	sleep 5
+	sleep 10
 	for i in `seq 1 80`; do carouselAdvance; sleep 1 ; done
-	carouselOff
 	shieldsDown
-	allOff
+	sleep 1	
+	carouselOff
 	sleep 60
-done)&
+done)
